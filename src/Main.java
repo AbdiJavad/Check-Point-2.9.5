4a46d960f31d14546a44baaa1ba93a7164117a48
AbdiJavad <Mirjavadabdi@gmail.com>
2024-02-04 00:24:38 +0330

    Updated,Changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        
         System.out.println(2 * (5 / 2 + 5 / 2));
         System.out.println(2 * 5 / 2 + 2 * 5 / 2);
         System.out.println(2 * (5 / 2));
